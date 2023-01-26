8aa8c102f0423ad034dc55dfd5b72a2f8a535a5a
Joao Gosmani <joaogosmani@gmail.com>
2023-01-26 19:48:32 -0300

    Criando a página de login

---
+++ src/main/java/br/com/joaogosmani/jgprojetos/controllers/UsuarioController.java
@@ -0,0 +1,14 @@
+package br.com.joaogosmani.jgprojetos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "usuario/login";
+    }
+
+}
