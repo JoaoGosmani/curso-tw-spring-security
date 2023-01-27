fdf56e98d420670639c46273a5541b483fcd7a98
Joao Gosmani <joaogosmani@gmail.com>
2023-01-27 16:02:57 -0300

    Criando a página de perfil do usuário logado

--- src/main/java/br/com/joaogosmani/jgprojetos/controllers/UsuarioController.java
+++ src/main/java/br/com/joaogosmani/jgprojetos/controllers/UsuarioController.java
@@ -1,14 +1,34 @@
 package br.com.joaogosmani.jgprojetos.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.joaogosmani.jgprojetos.models.Funcionario;
+import br.com.joaogosmani.jgprojetos.repositories.FuncionarioRepository;
 
 @Controller
 public class UsuarioController {
 
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
     @GetMapping("/login")
     public String login() {
         return "usuario/login";
+    }
+
+    @GetMapping("/perfil")
+    public ModelAndView perfil(Principal principal) {
+        ModelAndView modelAndView = new ModelAndView("usuario/perfil");
+
+        Funcionario usuario = funcionarioRepository.findByEmail(principal.getName()).get();
+        modelAndView.addObject("usuario", usuario);
+
+        return modelAndView;
     }
 
 }
